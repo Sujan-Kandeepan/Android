46341300631bf08ec06b5849aa1aedfa096d3f0e
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-03-09 13:23:32 -0500

    Basic app with marker and shape on map

---
+++ HeatmapFormat/app/src/main/java/com/example/sujan/heatmapformat/MapsActivity.java
@@ -0,0 +1,65 @@
+package com.example.sujan.heatmapformat;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    private final static int ALPHA_ADJUSTMENT = 0x77000000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in New York and move the camera
+        LatLng newyork = new LatLng(40.7484, -73.9857);
+        mMap.addMarker(new MarkerOptions().position(newyork).title("Marker in New York"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newyork, 15));
+
+        ArrayList<LatLng> shape = new ArrayList<>();
+        shape.add(new LatLng(newyork.latitude - 0.001,newyork.longitude - 0.001));  // Should match last point
+        shape.add(new LatLng(newyork.latitude + 0.001,newyork.longitude - 0.001));
+        shape.add(new LatLng(newyork.latitude + 0.001,newyork.longitude + 0.001));
+        shape.add(new LatLng(newyork.latitude - 0.001,newyork.longitude + 0.001));
+        shape.add(new LatLng(newyork.latitude - 0.001,newyork.longitude - 0.001));  // Should match first point
+
+        mMap.addPolygon(new PolygonOptions()
+                .addAll(shape)
+                .fillColor(Color.BLUE - ALPHA_ADJUSTMENT)
+                .strokeColor(Color.BLUE)
+                .strokeWidth(5));
+    }
+}
