ccf4da91073d19c37c6ec5b1fbb5b333f7b6dc50
Sujan Kandeepan <kandeeps@mcmaster.ca>
2018-03-15 14:40:15 -0400

    More meaningful comments

--- SafeDriver/app/src/main/java/cas/xb3/safe_driver/MapsActivity.java
+++ SafeDriver/app/src/main/java/cas/xb3/safe_driver/MapsActivity.java
@@ -24,15 +24,19 @@ import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    // Define UI elements
     LinearLayout linearLayout;
     EditText startEditText;
     EditText endEditText;
     Button navigateButton;
 
+    // Map instance variable
     private GoogleMap mMap;
 
+    // Colour adjustment constant
     private final static int ALPHA_ADJUSTMENT = 0x77000000;
 
+    // Define behaviour on app startup
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +47,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    // Define and adjust UI elements upon display
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         linearLayout = findViewById(R.id.linearLayout);
@@ -55,18 +60,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         endEditText.setWidth(startEditText.getWidth());
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // Define behaviour once map is fetched and ready
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        // Fetch and initialize map
         mMap = googleMap;
 
         // Add a marker in New York and move the camera
@@ -75,6 +72,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newyork, 15));
 
+        // Plot nine sample shapes
         for (int i = -1; i <= 1; i++) {
             for (int j = -1; j <= 1; j++) {
                 LatLng point = new LatLng(newyork.latitude + i * 0.002,
@@ -89,16 +87,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    // Draw collision cluster polygon, given perimeter points and intensity
     public void drawPolygon(ArrayList<LatLng> shape, int intensity) {
+        // Set colour by intensity, more red given higher value
         int colourShift = intensity * 0x1100; // Varying intensity between 0-8 collision points
         if (colourShift > 0x8800) {
             colourShift = 0x8800;
         }
         colourShift = 0x8800 - colourShift;
 
+        // Log colour values
         Log.i("Intensity", Integer.toString(intensity));
         Log.i("Colour shift", Integer.toString(colourShift));
 
+        // Draw polygon on map
         shape.add(shape.get(0));
         mMap.addPolygon(new PolygonOptions()
                 .addAll(shape)
@@ -107,40 +109,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .strokeWidth(5));
     }
 
+    // Button pressed, start navigation
     public void navigate(View view) {
+        // Extract coordinate information from text fields
         String startPoint = startEditText.getText().toString();
         String endPoint = endEditText.getText().toString();
         double startLat, startLng, endLat, endLng;
+
+        // Attempt to start navigation with given data
         try {
+            // Obtain coordinate information from inputted text
             startLat = Double.parseDouble(startPoint.split(",")[0]);
             startLng = Double.parseDouble(startPoint.split(",")[1]);
             endLat = Double.parseDouble(endPoint.split(",")[0]);
             endLng = Double.parseDouble(endPoint.split(",")[1]);
 
+            // Checking if latitude in range
             boolean inRange = true;
             for (double pt : new double[] {startLat, endLat}) {
                 if (pt < -90 || pt > 90) {
                     inRange = false;
                 }
             }
+
+            // Checking if longitude in range
             for (double pt : new double[] {startLng, endLng}) {
                 if (pt < -180 || pt > 180) {
                     inRange = false;
                 }
             }
 
+            // Navigate if coordinates are within map bounds
             if (inRange) {
                 Toast.makeText(this, "Navigate!", Toast.LENGTH_SHORT).show();
             }
+
+            // Show error message if coordinates out of bounds
             else {
                 Toast.makeText(this, "Location coordinates out of range",
                         Toast.LENGTH_SHORT).show();
             }
-        } catch (Exception e) {
+        }
+
+        // Invalid text, issues parsing if empty or formatted incorrectly
+        catch (Exception e) {
+            // One or both text fields empty, display error message
             if (startPoint.isEmpty() || endPoint.equals("")) {
                 Toast.makeText(this, "One or both text fields empty",
                         Toast.LENGTH_SHORT).show();
             }
+
+            // Invalid location input format, display error message
             else {
                 Toast.makeText(this, "Enter start/end points with format: lat,lng",
                         Toast.LENGTH_LONG).show();
